2e17e4f22672dac973890c801ced255162bf6241
tungdt <tungdothanh.hp@gmail.com>
2025-05-22 15:41:08 +0700

    Update init-primary.sh

--- init-primary.sh
+++ init-primary.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
-echo "host replication all 0.0.0.0/0 md5" >> "$PGDATA/pg_hba.conf"
-echo "wal_level = replica" >> "$PGDATA/postgresql.conf"
-echo "max_wal_senders = 10" >> "$PGDATA/postgresql.conf"
-echo "max_replication_slots = 10" >> "$PGDATA/postgresql.conf"
-echo "hot_standby = on" >> "$PGDATA/postgresql.conf"
-echo "synchronous_commit = on" >> "$PGDATA/postgresql.conf"
+if [ -d "$PGDATA" ]; then
+  echo "host replication all 0.0.0.0/0 md5" >> "$PGDATA/pg_hba.conf"
+  echo "wal_level = replica" >> "$PGDATA/postgresql.conf"
+  echo "max_wal_senders = 10" >> "$PGDATA/postgresql.conf"
+  echo "max_replication_slots = 10" >> "$PGDATA/postgresql.conf"
+  echo "hot_standby = on" >> "$PGDATA/postgresql.conf"
+  echo "synchronous_commit = on" >> "$PGDATA/postgresql.conf"
+else
+  echo "Error: PGDATA directory not found" >&2
+  exit 1
+fi
